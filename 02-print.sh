a494527e034be1e626835a3f773ad75b9c7f8f3f
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-05-27 16:46:05 +0530

    just priting the line using colours

--- 02-print.sh
+++ 02-print.sh
@@ -16,14 +16,8 @@ echo -e "Hello,\n welcome to my world"
 echo -e "\nHello,\n\nWelcome"
 
 #syntax: \t to print a tab space
-echo -e "\tA news anchor's attempt to report on Cyclone Yaas and its
-        impact in Odisha quickly devolved into a comedy segment of sorts
-        when the man he stopped for an interview gave him a brutally honest
-        reply to his query. Parts of Odisha have been experiencing
-        high-speed windsand heavy rains due to the "very severe cyclonic storm" Yaas.
-         In such inclement weather conditions, the reporter for Naxatra News was
-         surprised to see people out and about - and questioned one man about why he
-          chose to step out in the midst of a cyclone."
+echo -e "\tpassion is not a job, a sport or hobby. It is the full force of your attention,
+         an energy you give to whatever is right in front of you. -Terri."
 
 ## print in colours
 #syntax:echo -e "\e[COLORmMessage"
