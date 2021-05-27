04f97ccbfec058aaaed366e68a3e398999b273b5
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-05-27 16:27:37 +0530

    Just printing the line using echo

--- 02-print.sh
+++ 02-print.sh
@@ -12,3 +12,13 @@ echo Tommy
 # syntax: echo -e "line1/nline2"
 
 echo -e "Hello,\n welcome to my world"
+
+echo -e "\nHello,\n\nWelcome"
+
+echo -e "A news anchor's attempt to report on Cyclone Yaas and its
+        impact in Odisha quickly devolved into a comedy segment of sorts
+        when the man he stopped for an interview gave him a brutally honest
+        reply to his query. Parts of Odisha have been experiencing high-speed winds          and heavy rains due to the "very severe cyclonic storm" Yaas.
+         In such inclement weather conditions, the reporter for Naxatra News was
+         surprised to see people out and about - and questioned one man about why he
+          chose to step out in the midst of a cyclone."
