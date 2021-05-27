656d006153da642a90038b0cf3438781565c74a5
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-05-27 16:32:13 +0530

    Just printing the line using echo

--- 02-print.sh
+++ 02-print.sh
@@ -19,7 +19,8 @@ echo -e "\nHello,\n\nWelcome"
 echo -e "\tA news anchor's attempt to report on Cyclone Yaas and its
         impact in Odisha quickly devolved into a comedy segment of sorts
         when the man he stopped for an interview gave him a brutally honest
-        reply to his query. Parts of Odisha have been experiencing high-speed winds          and heavy rains due to the "very severe cyclonic storm" Yaas.
+        reply to his query. Parts of Odisha have been experiencing
+        high-speed windsand heavy rains due to the "very severe cyclonic storm" Yaas.
          In such inclement weather conditions, the reporter for Naxatra News was
          surprised to see people out and about - and questioned one man about why he
           chose to step out in the midst of a cyclone."
