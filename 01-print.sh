7328fbd0ba6c098f275b8cd4e1c00a6c3f2deb99
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-05-27 15:55:02 +0530

    Just pprinting the line using echo in 02-print.sh

---
+++ 01-print.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+# echo  and printf command can be used to print some message on the screen
+
+# However we use echo command here, because it is simple and yet effective to all the needs.
+
+# syntax: echo Input message
+
+echo Tommy
