bbc47beccb6dbf74d533db29424e2affce3c15b2
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-05-31 17:34:22 +0530

    Adding sample function

--- 07-functions.sh
+++ 07-functions.sh
@@ -3,7 +3,11 @@
 ## Declare a function
 SAMPLE(){
   echo Welcome to SAMPLE function
+  echo value of cloud =$CLOUD
+  SERVER=NGINX
 }
 
 ## Access the function
+CLOUD=aws
 SAMPLE
+echo value for the server =$SERVER
