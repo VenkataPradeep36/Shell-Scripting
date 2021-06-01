26c03464014817d843bd955450152795bc8bea73
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-01 18:34:07 +0530

    Adding file if.sh

--- 08-if.sh
+++ 08-if.sh
@@ -4,9 +4,18 @@
 
 read -p "Enter your name :" username
 
-if [ $username == "Pradeep" ] ; then
+if [ $username == "pradeep" ] ; then
   echo  "Hey, user $username is root user"
 else
   echo "Hey, user $username is Normal user"
 fi
 
+## File comparison
+
+read -p "enter file name :" filename
+
+if [ -f "$filename" ]; then
+  echo "file exits"
+else
+  echo "file not exits"
+fi
