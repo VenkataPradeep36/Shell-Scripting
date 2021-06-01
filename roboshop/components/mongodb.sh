381a576b8443f28cb00c104da0c5ac9741e04602
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-01 19:59:17 +0530

    Written the MOngoDB script by installing and configuring MongoDB

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,25 @@
 #!bin/bash
 
-echo Installing MongoDB
+source components/common.sh
+## Deleting the previous log content before executing
+rm -f /tmp/roboshop.log
+
+HEAD "Setup the MongoDB yum repo file"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT $?
+
+HEAD "Installing Mongodb"
+yum install -y mongodb-org &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Start MongoDB service"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl start mongod &>>/tmp/roboshop.log
+STAT $?
+
+
