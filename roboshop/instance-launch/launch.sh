0820aab07131ba1a529ba5b6ed74efba120284cc
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-02 15:39:32 +0530

    Created launch file and added script to launch an instance using template

---
+++ roboshop/instance-launch/launch.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+COMPONENT=$1
+## This First agrument is storing in a standard variable which is component
+
+if [ -z "${COMPONENT}" ]; then
+  echo "Component input is needed"
+  exit 1
+fi
+
+LID=lt-0e325cdb1fd0c644e
+LVER=1
+
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}}]" | jq
