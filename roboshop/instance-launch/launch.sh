d2d667eda2d0e4315f02bacd585c9c802ffc6440
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-02 18:39:45 +0530

    Created launch file and added script to launch an instance using template. If instance is already there it will say display message

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -11,4 +11,18 @@ fi
 LID=lt-0e325cdb1fd0c644e
 LVER=1
 
+## Validate if instance is already there
+INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
+
+if [ "{$INSTANCE_STATE}" = "running" ]; then
+   echo "Instance is already exist!"
+   exit 0
+fi
+
+if [ "{$INSTANCE_STATE}" = "stopped" ]; then
+   echo "Instance is already exist!"
+   exit 0
+fi
+
+## To Launch an instance from launch template
 aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
