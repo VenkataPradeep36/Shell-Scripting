7702f1d09353836d974669270fdf6dc5b1912202
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-03 18:44:26 +0530

    Added mongodb service to roboshop

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -4,7 +4,7 @@ source components/common.sh
 ## Deleting the previous log content before executing
 rm -f /tmp/roboshop.log
 
-HEAD "Setup MongoDB yum repo file"
+HEAD "Setup MongoDB yum repo file\t"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -13,7 +13,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 STAT $?
 
-HEAD "Install Mongodb\t"
+HEAD "Install Mongodb\t\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
 STAT $?
 
@@ -22,12 +22,12 @@ HEAD "Update Listen address in Config File"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 STAT $?
 
-HEAD "Start MongoDB service"
+HEAD "Start MongoDB service\t"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl start mongod &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Download Schema from GITHUB"
+HEAD "Download Schema from GITHUB\t"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
@@ -37,7 +37,7 @@ cd /tmp
 unzip -o mongodb.zip &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Load the Schema"
+HEAD "Load the Schema\t\t\t"
 cd mongodb-main
 mongo < catalogue.js &>>/tmp/roboshop.log && mongo < users.js &>>/tmp/roboshop.log
 STAT $?
