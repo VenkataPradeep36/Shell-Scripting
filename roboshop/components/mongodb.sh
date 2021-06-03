d86e7aacb2d77fc21d5d7d3068bace9205f8fd6e
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-03 18:35:48 +0530

    Added mongodb script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -17,9 +17,27 @@ HEAD "Install Mongodb\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
 STAT $?
 
+
+HEAD "Update Listen address in Config File"
+sed -e -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
 HEAD "Start MongoDB service"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl start mongod &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Download Schema from GITHUB"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
 
+
+HEAD "Extract the downloaded archive"
+cd /tmp
+unzip -o mongodb.zip &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Load the Schema"
+cd mongodb-main
+mongo < catalogue.js &>>/tmp/roboshop.log && mongo < users.js &>>/tmp/roboshop.log
+STAT$?
