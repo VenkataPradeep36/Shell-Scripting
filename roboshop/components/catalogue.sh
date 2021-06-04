e134fb03059b830e8621b96cdbff7a88a84f7406
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-04 18:33:51 +0530

    added rm -rf

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -24,12 +24,7 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
 STAT $?
 
 HEAD "Extract the Downloaded Archive"
-##cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
-cd /home/roboshop
-rm -rf catalogue && unzip /tmp/catalogue.zip
-
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
+cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
 STAT $?
 
 
