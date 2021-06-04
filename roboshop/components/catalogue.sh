f9159803fa547b1e48d41112413ddc961b42b7d0
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-04 18:39:42 +0530

    added rm -rf

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -23,15 +23,16 @@ HEAD "Download App From GitHub"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
+## In this its unzipping the file and moving catalogue-main to catalogue, but catalogue dir is already there if you run for second time so by avoiding this we have to remove the content before executing
+
 HEAD "Extract the Downloaded Archive"
 cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
 STAT $?
-
 
 HEAD " Install NodeJs Dependencies"
 ## We need to run this as normal user but to avoiding this we using unsafe perm
 cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
 STAT $?
 
-
+HEAD "Fix permissions to Appp User"
 
