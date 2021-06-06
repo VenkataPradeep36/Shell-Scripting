3e373e638756e0e62ee8a248b51952d9aa14a766
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-06 08:45:24 +0530

    added script to redis

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -3,10 +3,10 @@
 source components/common.sh
 ## Deleting the previous log content before executing
 rm -f /tmp/roboshop.log
-
 set-hostname redis
 
 HEAD "SetUp Redis Repos"
 yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/roboshop.log && yum-config-manager --enable remi &>>/tmp/roboshop.log
 STAT $?
+
 # yum install redis -y
