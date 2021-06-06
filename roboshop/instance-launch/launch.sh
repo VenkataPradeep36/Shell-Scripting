972647906004e8583ff70a45db8de771d749cc46
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-06 07:24:32 +0530

    added record.json

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -13,7 +13,7 @@ LVER=1
 
 DNS_UPDATE(){
   PRIVATEIP= $(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
-  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json>/tmp/record.json
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z00392432B47AFBZ92BGI --change-batch file:///tmp/record.json | jq
 }
 
@@ -33,6 +33,4 @@ fi
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 sleep 30
-
-
 DNS_UPDATE
