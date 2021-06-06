76981f24a60c4fbd94552a8ef148de7dd2b6dfaa
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-06 18:20:21 +0530

    added rabbitmq script

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -10,17 +10,17 @@ yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erl
 STAT $?
 
 HEAD "SetUp RabbitMQ Yum Repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash >>tmp/roboshop.log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>tmp/roboshop.log
 STAT $?
 
 HEAD "Install RabbitMQ Server"
-yum install rabbitmq-server -y >>tmp/roboshop.log
+yum install rabbitmq-server -y &>>tmp/roboshop.log
 STAT $?
 
 HEAD "Start RabbitMQ Server"
-systemctl enable rabbitmq-server >>tmp/roboshop.log && systemctl start rabbitmq-server >>tmp/roboshop.log
+systemctl enable rabbitmq-server &>>tmp/roboshop.log && systemctl start rabbitmq-server &>>tmp/roboshop.log
 STAT $?
 
 HEAD "Create Application User"
-rabbitmqctl add_user roboshop roboshop123 >>tmp/roboshop.log && rabbitmqctl set_user_tags roboshop administrator >>tmp/roboshop.log && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" >>tmp/roboshop.log
+rabbitmqctl add_user roboshop roboshop123 &>>tmp/roboshop.log && rabbitmqctl set_user_tags roboshop administrator &>>tmp/roboshop.log && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>tmp/roboshop.log
 STAT $?
