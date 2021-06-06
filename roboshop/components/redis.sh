a68654f880b054adcb5ca844024308b07b05f157
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-06 08:53:18 +0530

    added script to redis

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -5,11 +5,11 @@ source components/common.sh
 rm -f /tmp/roboshop.log
 set-hostname redis
 
-HEAD "SetUp Redis Repos"
+HEAD "SetUp Redis Repos\t\t\t"
 yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/roboshop.log && yum-config-manager --enable remi &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Install Redis"
+HEAD "Install Redis\t\t\t"
 yum install redis -y &>>/tmp/roboshop.log
 STAT $?
 
@@ -17,6 +17,6 @@ HEAD "Update Listen Address in Redis Config"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 STAT $?
 
-HEAD "Start Redis Update"
+HEAD "Start Redis Update\t\t\t"
 systemctl enable redis &>>/tmp/roboshop.log && systemctl start redis &>>/tmp/roboshop.log
 STAT $?
