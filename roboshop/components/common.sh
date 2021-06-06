9c1d8fbf445ac50e74d16dadacbe56d823abbcd6
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-06 22:01:23 +0530

    added looping concept for frontend

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -31,7 +31,7 @@ APP_USER_ADD() {
 
 SETUP_SYSTEMD() {
   ##Inserting mongodb ip address by using -i command and moving the file
-  HEAD "SetUp the SyetemD service file"
+  HEAD "SetUp the SystemD service file"
   sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/$1/systemd.service  && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
   STAT $?
 
@@ -74,6 +74,7 @@ NODEJS() {
   STAT $?
 
   FIX_APP_CONTENT_PERM
+  ## need to be Understand
   SETUP_SYSTEMD "$1"
 
 }
@@ -111,6 +112,7 @@ PYTHON3() {
   HEAD "Update App Configuration"
   sed -i -e "/uid/ c uid=${USER_ID}" -e "/gid/ c gid=${GROUP_ID}" /home/roboshop/$1/$1.ini
   STAT $?
+  ## need to be understand
   SETUP_SYSTEMD $1
 
 }
