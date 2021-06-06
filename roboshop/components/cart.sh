90d9f5a063fd4b1195daf40d4e1890588e34d318
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-06 15:47:37 +0530

    added sed catalogue

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,8 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
 source components/common.sh
 ## Deleting the previous log content before executing
 rm -f /tmp/roboshop.log
-set-hostname cart
+#set-hostname cart
 
 NODEJS "cart"
