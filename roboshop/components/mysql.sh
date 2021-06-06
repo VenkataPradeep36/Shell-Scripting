dbf2039abc22c1ec243317b2bf2d6d6b89a4e578
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-06 16:31:36 +0530

    added sed catalogue

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -21,11 +21,10 @@ HEAD "Start MYSQL Service"
 systemctl enable mysqld &>>/tmp/roboshop.log && systemctl start mysqld &>>/tmp/roboshop.log
 STAT $?
 
-DEAFULT_ROOT=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+DEF_PASS=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
 uninstall plugin validate_password;" >/tmp/db.sql
 
-HEAD "Reset MySQL Password"
-mysql -uroot -p"$(DEAFULT_ROOT)" <tmp/db.sql &>>tmp/roboshop.log
+HEAD "Reset MYSQL Password"
+mysql -uroot -p"${DEF_PASS}" <tmp/db.sql &>>/tmp/roboshop.log
 STAT $?
-
