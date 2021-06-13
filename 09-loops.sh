97d0ed37b97a52e1b15e6119e048a2f6e0f9b37d
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-06-13 15:55:36 +0530

    added dns names in Inv-roboshop file

--- 09-loops.sh
+++ 09-loops.sh
@@ -13,3 +13,6 @@ for Language in Java Python Go ; do
   echo Language name = $Language
   sleep 1
 done
+
+
+for i in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do echo -e "[$(echo $i | tr [a-z [A-Z]])]\n$i.roboshop.internal" ;done
